e4226cd330784bb0dcf7c19112267bc58bfbbc14
HelloWorld7894 <danpojhan@protonmail.com>
2023-02-26 11:24:29 +0100

    Neural Net into production, done

--- src/main.py
+++ src/main.py
@@ -2,8 +2,15 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import math
+import torch
 import os
+import time
 
+#neural network things
+import process_dataset
+import Model
+
+#Mediapipe setup
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
@@ -11,6 +18,9 @@ mp_face_mesh = mp.solutions.face_mesh
 #via canonical_face_model_uv_visualization.png
 face_landmarks_lower = [78, 95, 88, 178, 87, 14, 317, 405, 318, 324, 308]
 face_landmarks_upper = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
+
+#Pytorch setup
+SMILENet = torch.load(os.getcwd() + "/model/SMILENet0.pth") #best model yet
 
 def DrawSmile(arr_y, arr_x, col):
     for i in range(arr_x.shape[0]):
@@ -69,6 +79,19 @@ with mp_face_mesh.FaceMesh(
         h, w, c = image.shape
         blank = np.zeros((h, w, c))
         ROI = np.zeros((roi_size, roi_size, c))
+        OUT = np.zeros((8, 8, 3))
+
+        #create cool looking smiley face
+        OUT[2:6, 0, :] = (255, 0, 0)
+        OUT[2:6, 7, :] = (255, 0, 0)
+        OUT[0, 2:6, :] = (255, 0, 0)
+        OUT[7, 2:6, :] = (255, 0, 0)
+        OUT[6, 6, :] = (255, 0, 0)
+        OUT[1, 1, :] = (255, 0, 0)
+        OUT[1, 6, :] = (255, 0, 0)
+        OUT[6, 1, :] = (255, 0, 0)
+        OUT[2, 5, :] = (0, 255, 0)
+        OUT[2, 2, :] = (0, 255, 0)
 
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
@@ -172,14 +195,42 @@ with mp_face_mesh.FaceMesh(
                     pad_y = int((roi_size - ROI_propose.shape[0]) / 2)
 
                     ROI[pad_y:ROI_propose.shape[0]+pad_y, :] = ROI_propose
+                    ROI = ROI.astype(dtype=np.uint8)
+
+                    ROI_tensor = process_dataset.Convert_To_Tensor(ROI)
+                    ROI_tensor = torch.unsqueeze(ROI_tensor, 0)
+                    output = SMILENet(ROI_tensor)
+                    pred = int(torch.max(output,1)[1][0].to(torch.uint8))
                     
+                    match pred:
+                        case 0:
+                            OUT[6, 3:5, :] = (0, 0, 255)
+                            OUT[5, 2, :] = (0, 0, 255)
+                            OUT[5, 5, :] = (0, 0, 255)
+                        case 1:
+                            OUT[6, 3:5, :] = (0, 0, 255)
+                            OUT[5, 2:6, :] = (0, 0, 255)
+                        case 2:
+                            OUT[5, 2:6, :] = (0, 0, 255)
+                        case 3:
+                            OUT[6, 2:6, :] = (0, 0, 255)
+                            OUT[5, 1, :] = (0, 0, 255)
+                            OUT[5, 6, :] = (0, 0, 255)
+                        case 4:
+                            OUT[6, 2:6, :] = (0, 0, 255)
+                            OUT[5, 1, :] = (0, 0, 255)
+                            OUT[5, 6, :] = (0, 0, 255)
+                            OUT[4, 1:7, :] = (0, 0, 255)
+
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow('MediaPipe Face Mesh', image)
         #cv2.imshow('Smile approx', blank)
         cv2.imshow("Smile ROI", ROI)
+        cv2.imshow("CNN Output", OUT)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        
+        """
         elif cv2.waitKey(33) == ord('a'):
             count = 0
             for path in os.listdir(os.getcwd() + "/data/"):
@@ -190,5 +241,6 @@ with mp_face_mesh.FaceMesh(
             print(count)
             cv2.imwrite(os.getcwd() + "/data/" + str(count+1) + ".png", ROI)
             print("saved!")
+        """
 
 cap.release()
